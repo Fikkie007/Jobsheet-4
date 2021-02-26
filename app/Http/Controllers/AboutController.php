5d7da53b89e2ec7d762d454160cfc2057325e324
Fikkie007 <fikkieakbar48@gmail.com>
2021-02-26 14:23:22 +0700

    2_praktikum_web_luntung_dua

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about(){
+        echo"1941720079 & Fikkie Akbar H";
+    }
+    //
+}
